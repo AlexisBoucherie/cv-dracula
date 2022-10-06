f72a8367cd771d9c5608e478b4c618c9eddfebc6
Alexis Boucherie <alexis.boucherie@gmail.com>
2022-10-06 17:55:14 +0200

    start to fix desktop css

--- header.php
+++ header.php
@@ -1,9 +1,10 @@
 <header>
-    <div class="banner">
 
+    <div class="banner">
         <img class="photo-profil" src="img/dracula.png" alt="photo-profil" srcset="">
         <h1>Dracula</h1>
     </div>
+
     <div class="button-toggle">
         <div>
             <label class="switch">
@@ -16,8 +17,8 @@
                 <button class="fr" name="lang-fr">FR</button>
                 <button class="en" name="lang-en">EN</button>
             </form>
-
         </div>
+
     </div>
 </header>
 <h2>Technicien de prélèvement pour la banque de sang</h2>
